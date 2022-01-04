a91282b757747ff358ad64db0b0975ddd0d2571d
jnnykg <95724155+jnnykg@users.noreply.github.com>
2022-01-04 16:28:18 -0500

    Create keyValuePair.js

---
+++ keyValuePair.js
@@ -0,0 +1,31 @@
+
+function createKeyValues(){
+  
+  myKeyVal =  createStringDict({});  //LetterVal, Index
+  myKeyVal.create('A', '0');
+  myKeyVal.create('B', '1');
+  myKeyVal.create('C', '2');
+  myKeyVal.create('D', '3');
+  myKeyVal.create('E', '4');
+  myKeyVal.create('F', '5');
+  myKeyVal.create('G', '6');
+  myKeyVal.create('H', '7');
+  myKeyVal.create('I', '8');
+  myKeyVal.create('J', '9');
+  myKeyVal.create('K', '10');
+  myKeyVal.create('L', '11');
+  myKeyVal.create('M', '12');
+  myKeyVal.create('N', '13');
+  myKeyVal.create('O', '14');
+  myKeyVal.create('P', '15');
+  myKeyVal.create('Q', '16');
+  myKeyVal.create('R', '17');
+  myKeyVal.create('S', '18');
+  myKeyVal.create('T', '19');
+  myKeyVal.create('U', '20');
+  myKeyVal.create('V', '21');
+  myKeyVal.create('W', '22');
+  myKeyVal.create('X', '23');
+  myKeyVal.create('Y', '24');
+  myKeyVal.create('Z', '25');
+}
